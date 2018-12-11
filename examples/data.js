99d79e0f7a9e72917d75ca7dee658f3a2be2560a
Yeshwanth M <yeshwanth.m@seventablets.com>
2018-12-11 15:03:08 -0600

    More changes

--- examples/data.js
+++ examples/data.js
@@ -3,7 +3,7 @@ const { convertCSVToArray } = require('convert-csv-to-array');
 
 const readData = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(__dirname + "/kc_house_data.csv", opts = 'utf8', (err, fileData) => {
+    fs.readFile(__dirname + "/kc_house_data_1.csv", opts = 'utf8', (err, fileData) => {
       if (err) {
         console.log("Error:\n", err);
         reject(err);
@@ -20,8 +20,14 @@ const getData = async () => {
     separator: ',', // use the separator you use in your csv (e.g. '\t', ',', ';' ...)
   });
 
-  const data = convertedData.slice(1, convertedData.length);
-  return data;
+  const eighty_percent = Math.floor(convertedData.length * 0.8);
+//   console.log("test", convertedData[6]);
+  const train_data = convertedData.slice(1, eighty_percent);
+  const test_data = convertedData.slice(eighty_percent + 1, convertedData.length);
+  return {
+	  train_data,
+	  test_data
+  };
 }
 
 module.exports = getData;
--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,8 @@
 const math = require("mathjs");
 const pcorr = require("./../lib");
 const readData = require("./data")();
-readData.then(data => {
+readData.then(resp => {
+  const data = resp.train_data;
   const N = 100000
   const x_matrix = [];
   const variables = {};
@@ -16,38 +17,96 @@ readData.then(data => {
       inputs.push(key);
     };
   }
-  console.log('inputs :', inputs);
+  //   console.log('inputs :', inputs);
 
   data
     .slice(0, N)
-    .map(x => {
-      let x_matrix_row = [];
+    .map((x, index) => {
+      // let x_matrix_row = [];
       for (const key in x) {
         if (key === output_var) {
-          output.push(+x[key]);
+          output.push(parseFloat(+x[key]));
         } else if (!!variables[key]) {
           variables[key].push(+x[key]);
-          x_matrix_row.push(+x[key]);
+          // x_matrix_row.push(+x[key]);
         };
       }
-      x_matrix.push(x_matrix_row);
+      // x_matrix.push(x_matrix_row);
     });
-  console.log("x_matrix.length ", x_matrix.length);
-  console.log("output.length ", output.length);
+  // console.log("x_matrix.length ", x_matrix.length);
+  console.log("output.length ", output.length, inputs);
 
   /* Correlation Method */
-  const corrInput = [];
+  const corrInput = [output];
   inputs.map(input => {
     corrInput.push(variables[input]);
   });
 
-  const mat = pcorr(output, ...corrInput);
-  console.log("Correlation: \n", mat[0]);
+  const mat = pcorr(corrInput);
+  //   console.log("Correlation: \n", mat[0]);
+
+  let corrObj = {};
+  corrObj[output_var] = mat[0][0];
+
+  const dep_inputs = [];
+  inputs.map((input, index) => {
+    corrObj[input] = mat[0][index + 1];
+    if (Math.abs(corrObj[input]) > 0.3)
+      dep_inputs.push(input);
+  })
+
+  data.map(h => {
+    let x_matrix_row = [];
+    for (const key in h) {
+			if (dep_inputs.indexOf(key) >= 0) {
+				x_matrix_row.push(+h[key]);
+			}
+		}
+		x_matrix.push(x_matrix_row);
+  })
+
+  console.log({ output_var }, "corrObj ", corrObj);
 
   /* Linear Regression Method */
   const x_transpose = math.transpose(x_matrix);
   const inv = math.inv(math.multiply(x_transpose, x_matrix));
   const coeff = math.multiply(inv, x_transpose, output);
-  console.log("\ncoeff:", coeff);
 
+  let coeffObj = {};
+  //   coeffObj[output_var] = mat[0][0];
+  dep_inputs.map((input, index) => {
+    coeffObj[input] = coeff[index];
+  });
+
+  // console.log("\ncoeff:", coeff);
+  console.log("\ncoeffObj:", coeffObj);
+
+  const train_data = resp.train_data;
+  let sq_err = sqErrorMethod(train_data, coeffObj, output_var, 0);
+  const avg_err = Math.sqrt(sq_err) / train_data.length;
+  console.log("Final sq_err ", sq_err, avg_err);
+
+  // const test_data = resp.test_data;
+  // let sq_err_2 = sqErrorMethod(test_data, coeffObj, output_var, -avg_err);
+  // const avg_err_2 = Math.sqrt(sq_err_2) / test_data.length;
+
+  // console.log("Final sq_err 2", sq_err_2, avg_err_2);
 });
+
+const sqErrorMethod = (data, coeffObj, output_var, err) => {
+  let sq_err = 0;
+  console.log("err ", err);
+  for (let i = 0; i < data.length; i++) {
+    let output_i = data[i][output_var];
+    let calc_output = err;
+    for (const key in coeffObj) {
+      calc_output += data[i][key] * coeffObj[key];
+    }
+    sq_err += Math.pow(output_i - calc_output, 2);
+    if (i < 10) {
+      console.log("Outputs:", output_i, calc_output, sq_err);
+    }
+  }
+
+  return sq_err;
+}
